92153cb563915b7ba5180e7d43a0414c6d081774
RamiKrispin <ramkrisp@umich.edu>
2020-11-25 15:49:53 -0800

    update the data

--- R/dataviz.R
+++ R/dataviz.R
@@ -1,16 +1,34 @@
 #' Sankey Plot with Plotly
-#' @details A customized function for plotting sankey plots with Plotly
+#' @details A customized function for data transformation and plotting sankey plot with Plotly
 #' @export
-#' @param x A data.frame input
-#' @param cols A vector of columns names
-#' @param num_col The numeric column name
+#' @param x A data.frame input, must have at least two categorical columns and one numeric column
+#' @param cat_cols A  vector of at least two categorical columns names
+#' @param num_col A single numeric column name
+#' @param title Optional, string to pass to plotly layout title function
 
 
-sankey_ly <- function(x, cols, num_col){
-  map <- function(x, cols){
+sankey_ly <- function(x, cat_cols, num_col, title = NULL){
+  `%>%` <- magrittr::`%>%`
+  index <- NULL
+
+  # Error handeling
+  if(!is.data.frame(x)){
+    stop("The input object is not a valid data.frame")
+  } else if(!all(c(cat_cols, num_col) %in% names(x))){
+    stop("One or more column names does not matching to the names of the input object")
+  } else if(length(num_col) != 1){
+    stop("Cannot define more than one numeric column with the num_col argument")
+  } else if(length(cat_cols) < 2){
+    stop("Must define at least two categorical columns with the cat_cols argument")
+  } else if(!is.null(title) && !is.character(title)){
+    stop("The title argument is not valid character object")
+  }
+
+
+  map <- function(x, cat_cols){
     unique_cat <- map_df <- NULL
     x <- as.data.frame(x)
-    for(i in cols){
+    for(i in cat_cols){
       unique_cat <- c(unique_cat, base::unique(x[, i]))
     }
 
@@ -20,14 +38,14 @@ sankey_ly <- function(x, cols, num_col){
     return(map_df)
   }
 
-  map <- map(x, cols)
-  df <- lapply(1:(base::length(cols) - 1), function(i){
+  map <- map(x, cat_cols)
+  df <- lapply(1:(base::length(cat_cols) - 1), function(i){
     df <- NULL
     df <- x %>%
-      dplyr::group_by_(s = cols[i], t = cols[i + 1]) %>%
-      dplyr::summarise_(.dots = setNames(paste("sum(", num_col, ",na.rm = TRUE)", sep = ""), "total")) %>%
-      dplyr::left_join(map %>% dplyr::select(s = cat, source = index)) %>%
-      dplyr::left_join(map %>% dplyr::select(t = cat, target = index))
+      dplyr::group_by_(s = cat_cols[i], t = cat_cols[i + 1]) %>%
+      dplyr::summarise_(.dots = stats::setNames(paste("sum(", num_col, ",na.rm = TRUE)", sep = ""), "total")) %>%
+      dplyr::left_join(map %>% dplyr::select(s = cat, source = index), by = "s") %>%
+      dplyr::left_join(map %>% dplyr::select(t = cat, target = index), by = "t")
 
     return(df)
   }) %>% dplyr::bind_rows()
@@ -55,5 +73,10 @@ sankey_ly <- function(x, cols, num_col){
       value =  df$total
     )
   )
+
+  if(!is.null(title)){
+    p <- p %>% plotly::layout(title = title)
+  }
+
   return(p)
 }

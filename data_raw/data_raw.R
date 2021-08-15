c81c76bb4eee94ddeedcea3df9c2b0f6aa6e4fff
RamiKrispin <ramkrisp@umich.edu>
2021-08-14 22:37:06 -0700

    update the data refresh function: - increase the nrow limit - review the datasets

--- data_raw/data_raw.R
+++ data_raw/data_raw.R
@@ -1,6 +1,6 @@
 # SFO Air Traffic Passengers
 `%>%` <- magrittr::`%>%`
-cmd <- "curl 'https://data.sfgov.org/resource/rkru-6vcg.json?$limit=25000' | jq -r '.[] |[.activity_period, .operating_airline, .operating_airline_iata_code,
+cmd <- "curl 'https://data.sfgov.org/resource/rkru-6vcg.json?$limit=50000' | jq -r '.[] |[.activity_period, .operating_airline, .operating_airline_iata_code,
 .published_airline, .published_airline_iata_code, .geo_summary, .geo_region,
 .activity_type_code, .price_category_code, .terminal, .boarding_area, .passenger_count] |  @tsv'"
 
@@ -16,12 +16,12 @@ sfo_passengers <- data.table::fread(cmd = cmd,
   as.data.frame()
 
 head(sfo_passengers)
-
+nrow(sfo_passengers)
 write.csv(sfo_passengers, "csv/sfo_passengers.csv", row.names = FALSE)
 usethis::use_data(sfo_passengers, overwrite = TRUE)
 
 # SFO Landing
-cmd_stats <- "curl 'https://data.sfgov.org/resource/fpux-q53t.json?$limit=30000' | jq -r '.[] |[.activity_period, .operating_airline, .operating_airline_iata_code,
+cmd_stats <- "curl 'https://data.sfgov.org/resource/fpux-q53t.json?$limit=50000' | jq -r '.[] |[.activity_period, .operating_airline, .operating_airline_iata_code,
 .published_airline, .published_airline_iata_code, .geo_summary, .geo_region,
 .landing_aircraft_type, .aircraft_body_type, .aircraft_manufacturer, .aircraft_model, .aircraft_version,
 .landing_count, .total_landed_weight] |  @tsv'"
@@ -40,6 +40,8 @@ sfo_stats <- data.table::fread(cmd = cmd_stats,
 
 head(sfo_stats)
 
+str(sfo_stats)
+max(sfo_stats$activity_period)
 write.csv(sfo_stats, "csv/sfo_stats.csv", row.names = FALSE)
 usethis::use_data(sfo_stats, overwrite = TRUE)
 
